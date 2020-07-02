0943e173a4ebc4c9d4819667c1be1c0e3efc8df5
neimad971 <damientalbot26@gmail.com>
2020-07-02 03:23:00 +0200

    adds asscoiation and validation on user_attribute_value
      - adds both bolongs_to association user and user_attribute
      - makes sure user and user_attribute are present and valid
      - adds spec for associations and validations

--- app/models/user_atrribute_value.rb
+++ app/models/user_atrribute_value.rb
@@ -1,5 +1,10 @@
 class UserAtrributeValue < ApplicationRecord
 
   validates :value, presence: true
-  
+  validates_presence_of :user_attribute
+  validates_presence_of :user
+
+  belongs_to :user_attribute
+  belongs_to :user
+
 end
--- spec/models/user_atrribute_value_spec.rb
+++ spec/models/user_atrribute_value_spec.rb
@@ -3,5 +3,12 @@ require 'rails_helper'
 RSpec.describe UserAtrributeValue, type: :model do
 
   it { should validate_presence_of :value }
+  it { should validate_presence_of :user_attribute }
+  it { should validate_presence_of :user }
+
+  it { should belong_to :user_attribute }
+  it { should belong_to :user }
+
+
 
 end
